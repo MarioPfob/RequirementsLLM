9caee21c4626f04c16bdcad839e1781ef45ce9a1
Mario Pfob <m.pfob@schwaebisch-media.de>
2024-03-16 15:25:25 +0100

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RequirementsLLM
+
+nfr und f_Test.txt --> original requirements file
+
+requirements.csv --> extracted the 625 line of requirements into a csv structure
+
+requirements.json --> python code transforms csv file into json, since it's a better structure for NLP and everything else
+
+main.py --> Python code
--- main.py
+++ main.py
@@ -7,26 +7,34 @@ import os
 
 
 def format_requirements():
-    # read requirements from csv
+    # Requirements will be loaded from CSV file into a pandas dataframe 
     df = pd.read_csv("requirements.csv", sep=";")
-    # df["RequirementText"] = df["RequirementText"].str.strip()  # --> sind die Whitespaces Absicht?
-    print(df.describe)
+    # df["RequirementText"] = df["RequirementText"].str.strip()  # --> TODO: Sind die Whitespaces Absicht?
+    # See if columns etc. look fine
+    print(df.describe())
+    # Write dataframe to JSON structure
     df.to_json("requirements.json", orient="records")
 
 
 def ask_gpt():
+    # Open json file (filled with requirements)
     f = open("requirements.json")
+    # Read the file and load them into a list of dicts
     data = json.load(f)
+    # Iterate through requirements
     for row in data[:1]:
         print("RequirementText: " + row["RequirementText"])
-        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+        # Instance of the OpenAI client in order to communicate with GPT
+        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))  # --> TODO: Der Key ist in einer .env Datei (nicht im Repo)
+        # Ask GPT to classify a single requirement specification
         response = client.chat.completions.create(
-        model="gpt-4-0125-preview",
+            model="gpt-4-0125-preview",
             messages=[
                 {"role": "user", "content": "Decide the class (choices: F, A, L, LF, MN, O, PE, SC, SE, US, FT, PO) of the following requirement specification: " + row["RequirementText"]},
             ]
         )
         print("Answer: " + response.choices[0].message.content)
+    # Close JSON file
     f.close()
 
 
